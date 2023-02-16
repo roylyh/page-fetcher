d29ba5256ffba4fa8dd69cda2195d847ad9500c7
roylyh <roy.louyihui@gmail.com>
2023-02-16 13:37:48 -0800

    refactoring the code - For edge cases

--- fetcher.js
+++ fetcher.js
@@ -1,23 +1,45 @@
-const request = require('request');
-const fs = require('fs');
+const request = require("request");
+const fs = require("fs");
+const readline = require("readline");
 
 const args = process.argv.slice(2);
 const url = args[0];
 const filePath = args[1];
 
-
 request(url, (error, response, body) => {
-  // console.log('error:', error); // Print the error if one occurred
+  if (error) {
+    console.log('There is an error with url:', error);
+    return;
+  }
+  // Print the error if one occurred
+  
   // console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
   // console.log('body:', body); // Print the HTML for the Google homepage.
+
+  // check if a file exists
+  fs.access(filePath, fs.F_OK, (err) => {
+    if (err) {
+      console.error("The file not exists and it will download now.");
+      fsWriteFile(filePath, body);
+      return;
+    }
+    const rl = readline.createInterface(process.stdin, process.stdout);
+    rl.question("Do you want to overwrite the file? Y for Yes ", (answer) => {
+      if (answer === "Y") {
+        fsWriteFile(filePath, body);
+      }
+      rl.close();
+    });
+  });
+});
+
+const fsWriteFile = function(filePath, body) {
   const length = body.length;
   fs.writeFile(filePath, body, (err) => {
     if (err) {
-      console.log(err);
+      console.log("There is an error on File Path: ", err);
     } else {
       console.log(`Downloaded and saved ${length} bytes to ${filePath}`);
     }
-
-  }
-  );
-});
+  });
+};
